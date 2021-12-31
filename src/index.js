373023dff49aa82a7a51dd49b572edc8d39f6643
Milos Stojanovic <stojanovic_m@outlook.com>
2021-12-31 23:25:02 +0100

    props

--- src/index.js
+++ src/index.js
@@ -2,22 +2,55 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./style.css";
 
+const inception = {
+  title: "Inception",
+  img: "https://flxt.tmsimg.com/assets/p7825626_p_v10_af.jpg",
+  releaseDate: "2010",
+  director: "Cristopher Nolan",
+};
+
+const interstellar = {
+  title: "Interstellar",
+  img: "https://m.media-amazon.com/images/M/MV5BZjdkOTU3MDktN2IxOS00OGEyLWFmMjktY2FiMmZkNWIyODZiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg",
+  releaseDate: "2014",
+  director: "Cristopher Nolan",
+};
+
 const MovieComponent = () => {
   return (
     <>
-      <Inception />
-      <div className="genre">
-        <p>Action, Mystery</p>
+      <div className="all">
+        <div className="book">
+          <Movie
+            title={inception.title}
+            img={inception.img}
+            releaseDate={inception.releaseDate}
+            director={inception.director}
+          />
+        </div>
+        <div className="book">
+          <Movie
+            title={interstellar.title}
+            img={interstellar.img}
+            releaseDate={interstellar.releaseDate}
+            director={interstellar.director}
+          />
+        </div>
       </div>
     </>
   );
 };
 
-const Inception = () => {
+const Movie = (props) => {
   return (
-    <div className="background">
-      <img className="img" alt="" />
-    </div>
+    <article>
+      <div>
+        <h2>Title: {props.title}</h2>
+        <img src={props.img} alt="" />
+        <p> Release date: {props.releaseDate}</p>
+        <p> Director: {props.director} </p>
+      </div>
+    </article>
   );
 };
 
